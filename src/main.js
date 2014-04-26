01f0f37a64e569f379b10051e0873c8fdf25ff64
Fernando Godinho <fppgodinho@gmail.com>
2014-04-26 16:39:40 +0100

    Just starting

---
+++ src/main.js
@@ -0,0 +1,20 @@
+var net = require('net');
+
+var clients = [];
+
+var server = net.createServer(function(socket) {
+    console.log('server connected', socket.remoteAddress);
+    socket.on('end', function() {
+        console.log('server disconnected');
+    });
+    socket.write('hello\r\n');
+    socket.pipe(socket);
+    socket.end();
+    
+    clients.push(socket);
+    
+});
+
+server.listen(4000, function() {
+  console.log('server bound');
+});
